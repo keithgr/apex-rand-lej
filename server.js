50f8459fde9b8566984b1b0d98e9abdb9cddbb18
Glitch (glitch-hello-node) <none>
2021-11-12 02:50:23 +0000

    🌦💐 Checkpoint ./server.js:9920076/780

--- server.js
+++ server.js
@@ -2,15 +2,24 @@
 const request = require('sync-request');
 
 // Regex
-const findNewSeasonPage = new RegExp(`\/games\/apex\-legends/\w+`, 'g');
+const findNewSeasonPage = new RegExp(`/games/apex-legends/\\w+`, 'g');
 
 // Main
 const homePage = getHttpContent('https://www.ea.com/games/apex-legends');
-const results = findNewSeasonPage.exec(homePage);
+const results = getMatches(homePage, findNewSeasonPage);
 console.log(results);
 
 // Functions
 function getHttpContent(url) {
   const res = request('GET', url);
   return res.getBody().toString();
+}
+
+function getMatches(strToSearch, regex) {
+  let arr;
+  let results = [];
+  while ((arr = regex.exec(strToSearch)) !== null) {
+    results.push(arr[0]);  
+  }
+  return results;
 }
