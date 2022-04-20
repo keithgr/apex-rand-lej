de1faa8900ac958c7ee537653ed463d809a8959c
Glitch (glitch-hello-node) <none>
2022-04-20 01:52:48 +0000

    🐺🏮 Checkpoint ./server.js:9920076/381

--- server.js
+++ server.js
@@ -247,6 +247,18 @@ function generateMetaData(settings) {
   };
 }
 
+
+
+// redirect HTTP to HTTPS
+app.use((request, response, next) => {
+  console.log(request);
+  if (!request.secure) {
+    console.log('https://' + request.headers.host + request.url);
+    return response.redirect('https://' + request.headers.host + request.url);
+  }
+  next();
+})
+
 // view homepage
 app.get("/", (request, response) => {
   let roomId = request.query.roomId;
